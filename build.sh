def7542bf0a512a2d68fdb93fa24a35a1369b5e5
richelbilderbeek <richel@richelbilderbeek.nl>
2016-03-20 11:40:31 +0100

    Use gcov-5 in build script

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ echo "Coverage run"
 qmake travis_qmake_gcc_cpp11_gcov_gcov.pro
 make
 ./travis_qmake_gcc_cpp11_gcov_gcov
-gcov main_gcov.cpp
-gcov do_magic.cpp
+gcov-5 main_gcov.cpp
+gcov-5 do_magic.cpp
 cat main_gcov.cpp.gcov
 cat do_magic.cpp.gcov
--- travis_qmake_gcc_cpp11_gcov_gcov.pro
+++ travis_qmake_gcc_cpp11_gcov_gcov.pro
@@ -7,7 +7,7 @@ QMAKE_CXXFLAGS += -Wall -Wextra -Weffc++ -Werror
 
 # gcov
 QMAKE_CXXFLAGS += -fprofile-arcs -ftest-coverage
-LIBS += -lgcov-5
+LIBS += -lgcov
 
 # C++11
 QMAKE_CXX = g++-5
