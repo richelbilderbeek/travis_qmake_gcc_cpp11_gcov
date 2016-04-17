46bc20c53da3a848fcb867cb5645c98779b3a732
richelbilderbeek <richel@richelbilderbeek.nl>
2016-04-17 15:51:14 +0200

    Simplified script

--- get_code_cov.sh
+++ get_code_cov.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
-GCOV_EXE=gcov-5 # gcov and g++ versions must match
-
-OBJ_FILES_PATH=.
-
-if [ ! -d "$OBJ_FILES_PATH" ]
-then
-  echo "Object file path not found at "$OBJ_FILES_PATH
-  exit 1
-fi
-
 for filename in `find . | egrep '\.cpp'`; 
 do 
-  echo "Getting codecov of "$filename
-  $GCOV_EXE -n -o $OBJ_FILES_PATH $filename > /dev/null; 
+  gcov-5 -n -o . $filename > /dev/null; 
 done
